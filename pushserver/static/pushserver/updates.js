78f5ca40cf8d36e5b60e6f1e16e7b361c054e506
Mitar <mitar.git@tnode.com>
2012-10-02 02:34:23 -0700

    Manually abort all pending requests on page close.

--- pushserver/static/pushserver/updates.js
+++ pushserver/static/pushserver/updates.js
@@ -1,156 +1,176 @@
 (function ($) {
-	var subscribedChannels = {};
-	var updatesProcessors = {};
+    var subscribedChannels = {};
+    var updatesProcessors = {};
 
-	function warn(message) {
-		if ((typeof window.console != 'undefined') && (typeof window.console.warn == 'function')) {
-			window.console.warn(message);
-		}
-	}
-	
-	function processUpdate(channel_name, data) {
-		if (!updatesProcessors[channel_name]) {
-			return;
-		}
+    function warn(message) {
+        if ((typeof window.console != 'undefined') && (typeof window.console.warn == 'function')) {
+            window.console.warn(message);
+        }
+    }
 
-		var processors = updatesProcessors[channel_name][data.type];
-		if (processors) {
-			$.each(processors, function (i, processor) {
-				processor(data);
-			});
-		}
-	}
+    function processUpdate(channel_name, data) {
+        if (!updatesProcessors[channel_name]) {
+            return;
+        }
 
-	function listenForUpdates(channel_name, channel_url) {
-		function listen(last_modified, etag) {
-			function delayedListen(new_last_modified, new_etag) {
-				setTimeout(function () {
-					listen(new_last_modified || last_modified, new_etag || '0');
-				}, 1000); // 1 second delay
-			}
+        var processors = updatesProcessors[channel_name][data.type];
+        if (processors) {
+            $.each(processors, function (i, processor) {
+                processor(data);
+            });
+        }
+    }
 
-			$.ajax({
-				'beforeSend': function (jqXHR) {
-					jqXHR.setRequestHeader('If-None-Match', etag);
-					jqXHR.setRequestHeader('If-Modified-Since', last_modified);
-				},
-				'xhrFields': {
-					'withCredentials': true
-				},
-				'url': channel_url,
-				'dataType': 'json',
-				'type': 'GET',
-				'timeout': 0,
-				'success': function (data, textStatus, jqXHR) {
-					var new_last_modified = jqXHR.getResponseHeader('Last-Modified');
-					try {
-						var new_etag = jqXHR.getResponseHeader('Etag');
-						if (new_etag === null) throw null;
-					}
-					catch (e) {
-						try {
-							// Chrome and other WebKit-based browsers do not (yet) support access to Etag
-							// so we try to find the same information in the Cache-Control field
-							new_etag = (/etag=(\S+)/.exec(jqXHR.getResponseHeader('Cache-Control')))[1];
-						}
-						catch (e) {}
-					}
+    function listenForUpdates(channel_name, channel_url) {
+        function listen(last_modified, etag) {
+            function delayedListen(new_last_modified, new_etag) {
+                setTimeout(function () {
+                    listen(new_last_modified || last_modified, new_etag || '0');
+                }, 1000); // 1 second delay
+            }
 
-					if (new_last_modified === null) {
-						warn("Last-Modified field is not available");
-					}
-					if (new_etag === null) {
-						warn("Etag field is not available");
-					}
+            $.ajax({
+                'beforeSend': function (jqXHR) {
+                    jqXHR.setRequestHeader('If-None-Match', etag);
+                    jqXHR.setRequestHeader('If-Modified-Since', last_modified);
+                },
+                'xhrFields': {
+                    'withCredentials': true
+                },
+                'url': channel_url,
+                'dataType': 'json',
+                'type': 'GET',
+                'timeout': 0,
+                'success': function (data, textStatus, jqXHR) {
+                    var new_last_modified = jqXHR.getResponseHeader('Last-Modified');
+                    try {
+                        var new_etag = jqXHR.getResponseHeader('Etag');
+                        if (new_etag === null) throw null;
+                    }
+                    catch (e) {
+                        try {
+                            // Chrome and other WebKit-based browsers do not (yet) support access to Etag
+                            // so we try to find the same information in the Cache-Control field
+                            new_etag = (/etag=(\S+)/.exec(jqXHR.getResponseHeader('Cache-Control')))[1];
+                        }
+                        catch (e) {}
+                    }
 
-					if (!data) {
-						warn("No data in push request");
-					}
-					else {
-						processUpdate(channel_name, data);
-					}
-					
-					if ((new_last_modified !== null) && (new_etag !== null)) {
-						listen(new_last_modified, new_etag);
-					}
-					else {
-						// TODO: Should we handle the error in some other manner?
-						delayedListen(new_last_modified, new_etag);
-					}
-				},
-				'error': function (jqXHR, textStatus, errorThrown) {
-					// TODO: Should we handle the error in some other manner?
-					delayedListen(last_modified, etag);
-				}
-			});
-		}
-		listen('Thu, 1 Jan 1970 00:00:00 GMT', '0');
-	}
+                    if (new_last_modified === null) {
+                        warn("Last-Modified field is not available");
+                    }
+                    if (new_etag === null) {
+                        warn("Etag field is not available");
+                    }
 
-	$.updates = {};
+                    if (!data) {
+                        warn("No data in push request");
+                    }
+                    else {
+                        processUpdate(channel_name, data);
+                    }
 
-	$.updates.subscribe = function (channels) {
-		 $.each(channels, function (name, url) {
-			if (!subscribedChannels[name] && url) {
-				subscribedChannels[name] = url;
-				listenForUpdates(name, url);
-			}
-		});
-	};
+                    if ((new_last_modified !== null) && (new_etag !== null)) {
+                        listen(new_last_modified, new_etag);
+                    }
+                    else {
+                        // TODO: Should we handle the error in some other manner?
+                        delayedListen(new_last_modified, new_etag);
+                    }
+                },
+                'error': function (jqXHR, textStatus, errorThrown) {
+                    // TODO: Should we handle the error in some other manner?
+                    delayedListen(last_modified, etag);
+                }
+            });
+        }
+        listen('Thu, 1 Jan 1970 00:00:00 GMT', '0');
+    }
 
-	$.updates.registerProcessor = function (channel_name, type, processor) {
-		if (!updatesProcessors[channel_name]) {
-			updatesProcessors[channel_name] = {};
-		}
+    $.updates = {};
 
-		if (!updatesProcessors[channel_name][type]) {
-			updatesProcessors[channel_name][type] = [];
-		}
+    $.updates.subscribe = function (channels) {
+         $.each(channels, function (name, url) {
+            if (!subscribedChannels[name] && url) {
+                subscribedChannels[name] = url;
+                listenForUpdates(name, url);
+            }
+        });
+    };
 
-		updatesProcessors[channel_name][type].push(processor);
-	};
+    $.updates.registerProcessor = function (channel_name, type, processor) {
+        if (!updatesProcessors[channel_name]) {
+            updatesProcessors[channel_name] = {};
+        }
 
-	$(document).ready(function () {
-		// Workaround for Firefox bug, based on http://bugs.jquery.com/ticket/10338#comment:13
-		var _super = $.ajaxSettings.xhr;
+        if (!updatesProcessors[channel_name][type]) {
+            updatesProcessors[channel_name][type] = [];
+        }
 
-		$.ajaxSetup({
-			xhr: function() {
-				var xhr = _super();
-				var getAllResponseHeaders = xhr.getAllResponseHeaders;
+        updatesProcessors[channel_name][type].push(processor);
+    };
 
-				xhr.getAllResponseHeaders = function () {
-					try {
-						var allHeaders = getAllResponseHeaders.call(xhr);
-						if (allHeaders) {
-							return allHeaders;
-						}
-					}
-					catch (e) {}
+    // Workaround for Firefox bug, based on http://bugs.jquery.com/ticket/10338#comment:13
+    var _super = $.ajaxSettings.xhr;
 
-					allHeaders = '';
-					$([
-						'Cache-Control',
-						'Content-Language',
-						'Content-Type',
-						'Expires',
-						'Last-Modified',
-						'Pragma',
-						'Etag'
-					]).each(function (i, headerName) {
-						try {
-							var headerValue = xhr.getResponseHeader(headerName);
-							if (headerValue) {
-								allHeaders += headerName + ': ' + headerValue + '\n';
-							}
-						}
-						catch (e) {}
-					});
-					return allHeaders;
-				};
+    $.ajaxSetup({
+        'xhr': function() {
+            var xhr = _super();
+            var getAllResponseHeaders = xhr.getAllResponseHeaders;
 
-				return xhr;
-			}
-		});
-	});
+            xhr.getAllResponseHeaders = function () {
+                try {
+                    var allHeaders = getAllResponseHeaders.call(xhr);
+                    if (allHeaders) {
+                        return allHeaders;
+                    }
+                }
+                catch (e) {}
+
+                allHeaders = '';
+                $.each([
+                    'Cache-Control',
+                    'Content-Language',
+                    'Content-Type',
+                    'Expires',
+                    'Last-Modified',
+                    'Pragma',
+                    'Etag'
+                ], function (i, headerName) {
+                    try {
+                        var headerValue = xhr.getResponseHeader(headerName);
+                        if (headerValue) {
+                            allHeaders += headerName + ': ' + headerValue + '\n';
+                        }
+                    }
+                    catch (e) {}
+                });
+                return allHeaders;
+            };
+
+            return xhr;
+        }
+    });
+
+    var ajaxRequests = [];
+    $(document).ajaxSend(function (event, jqXHR, ajaxOptions) {
+        ajaxRequests.push(jqXHR);
+    }).ajaxComplete(function (event, jqXHR, ajaxOptions) {
+        ajaxRequests = $.grep(ajaxRequests, function (request, i) {
+            return jqXHR !== request;
+        });
+    });
+
+    // We have to manually abort all pending requests because otherwise an
+    // Ajax error is triggered when browser closes the page which cannot be
+    // distinguished from real Ajax errors
+    // By aborting, you can check if Ajax error is "abort" and ignore it
+    function ajaxUnload (event) {
+        $.each(ajaxRequests, function(i, request) {
+            request.abort();
+        });
+        ajaxRequests = [];
+    }
+
+    $(window).bind('beforeunload', ajaxUnload).bind('unload', ajaxUnload);
 })(jQuery);
